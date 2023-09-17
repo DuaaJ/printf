6b38c27719c66b0bb750fc438eae7bf855439cea
nadertoti <nadertoti4@gmail.com>
2023-09-17 04:39:45 -0700

    nader

--- main.h
+++ main.h
@@ -114,16 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdarg.h>
-#include<unistd.h>
-#include<limits.h>
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-
-#endif
+#endif /* MAIN_H */
